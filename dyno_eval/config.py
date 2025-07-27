6ffda150cad9838db6d473598aa5654a1aea7db8
Maxx Wilson <jessemaxxwilson@utexas.edu>
2025-07-27 15:17:11 -0500

    tuning

--- dyno_eval/config.py
+++ dyno_eval/config.py
@@ -14,10 +14,10 @@ COLUMN_MAP = {
 MOTOR_KV = 1450
 GEAR_RATIO = 5.0
 MOTOR_INERTIA = 0.00000573 # Final estimated value
-FLYWHEEL_INERTIA = 0.0015
+FLYWHEEL_INERTIA = MOTOR_INERTIA
 
 # --- Analysis Parameters ---
 # We're reverting to this more flexible method for defining the analysis window.
 SETTLING_THRESHOLD = 0.95 # Clip the analysis when velocity reaches 95% of its peak
-STEADY_STATE_POINTS_TO_KEEP = 3 # Keep a few points of the plateau for context
+STEADY_STATE_POINTS_TO_KEEP = 0 # Keep a few points of the plateau for context
 VELOCITY_THRESHOLD = 10 # rev/s
